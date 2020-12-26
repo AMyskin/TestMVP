01e10fe6c4c457cc6cc976db4b02b5d0824e6035
Aleksandr Myskin <aleksandr.myskin@bsc-ideas.com>
2020-12-26 16:38:01 +0300

    Initial Commit

---
+++ TestMVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestMVP
+//
+//  Created by Alexander Myskin on 26.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
